f98ccc82468cf09b294c7bf1de6154931cd71cef
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-16 20:52:25 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
 count = 0 
 count2 = 1
 big_boi = 0
-while count<array.length do 
+while count && count2<array.length do 
 if big_boi = array[count]>array[count2]
   #array[count]>array[count2]
     
