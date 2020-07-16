a9f4ca44394fa119252db2c2f7c0bf8fb9ae708f
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-16 20:45:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,9 @@
 def find_max_value(array)
 count = 0 
+count2 = 2
 big_boi = 0
 while count<array.length do 
-  if big_boi =  array[count]>array[count]
+  if big_boi =  array[count]>array[count2]
   
   
   count += 1
